7ecde5ba72818e088c70ed5c4358a45e8e80a08b
PedroGiu13 <pgiussani.e@gmail.com>
2026-03-08 18:10:26 +0000

    config file - tickers + dates

---
+++ src/config.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+# Date Defaults
+START_DATE = "2015-01-01"
+END_DATE = datetime.now().strftime("%Y-%m-%d")
+
+
+# Stock Allocation
+SEMICONDUCTOR_TICKER = [
+    "NVDA",
+    "AMD",
+    "INTC",
+    "QCOM",
+    "AVGO",
+    "TXN",
+    "MU",
+    "MRVL",
+    "ADI",
+    "NXPI",
+    "ON",
+    "MPWR",
+    "SWKS",
+    "QRVO",
+    "CRUS",
+    "SLAB",
+    "MCHP",
+    "STM",
+    "SMTC",
+    "LSCC",
+]

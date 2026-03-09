9b14d326df372e97e15e53dbd513c014a431e90f
PedroGiu13 <pgiussani.e@gmail.com>
2026-03-09 00:26:03 +0000

    data inspection module

---
+++ src/data_inspection.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def df_structure():
+    pass
+
+
+def ticker_diagnostics():
+    pass
+
+
+def visual_diagnostics():
+    pass
+
+
+def stationarity_test():
+    pass
+
+
+def data_inspection():
+    pass

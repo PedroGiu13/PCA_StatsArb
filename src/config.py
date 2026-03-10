695efd69fe6ec8d61e66bedc2b01f21e0d1ef5d2
PedroGiu13 <pgiussani.e@gmail.com>
2026-03-10 22:27:15 +0000

    global variables added

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ END_DATE = datetime.now().strftime("%Y-%m-%d")
 
 
 # Stock Allocation
-SEMICONDUCTOR_TICKER = [
+TICKERS_LIST = [
     "NVDA",
     "AMD",
     "INTC",
@@ -28,3 +28,13 @@ SEMICONDUCTOR_TICKER = [
     "SMTC",
     "LSCC",
 ]
+
+UNIVERSE_SIZE = len(TICKERS_LIST)
+
+
+# Diagnostics Checks
+MIN_HISTORY_DAYS = 2500
+MISSING_DATA_THRESHOLD = 0.02
+LOW_CORR_THRESHOLD = 0.2
+EXTREME_THRESHOLD_10 = 0.10
+EXTREME_THRESHOLD_25 = 0.25
